796bd717277b14807640ea057bd6b1141c8e4364
Joel Yoshiya Foster <jyfoster@wisc.edu>
2023-05-25 15:52:59 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `joelyoshiya-personal-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
