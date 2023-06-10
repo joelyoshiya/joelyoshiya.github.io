e1a41516ffe9e6db290edd3be518b5103b7a1e29
Joel Yoshiya Foster <jyfoster@wisc.edu>
2023-06-10 13:50:07 -0500

    added a work page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,6 +16,14 @@ const Navbar = ({ siteTitle }) => {
         <li>
           <Link
             className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:bg-transparent md:dark:hover:text-green-500"
+            to="/work"
+          >
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:bg-transparent md:dark:hover:text-green-500"
             to="/projects"
           >
             Projects
---
+++ src/pages/work.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const WorkPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="Work">
+        <ul>
+          <li> Fetch</li>
+          <li> Olympic Broadcasting Services</li>
+          <li> DoIT Uw-Madison</li>
+        </ul>
+      </Layout>
+    </main>
+  );
+};
+
+export default WorkPage;
