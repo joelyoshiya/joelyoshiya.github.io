c533038a17bb388d82d7bab304224b073d1777d2
Joel Yoshiya Foster <jyfoster@wisc.edu>
2023-06-15 15:49:21 -0500

    cleaning up

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,3 @@
-// this will be the footer component, with slightly transparent and grey links to:
-// - my github: https://github.com/joelyoshiya
-// - my linkedin: https://www.linkedin.com/in/joel-yoshiya-foster/
-// - my email: joel.foster@gmail.com
-// - my resume: (will be a pdf)
-// - my spotify: https://open.spotify.com/user/1229025912?si=536fdfc74c854ef7
-// - my letterboxd: https://letterboxd.com/Mitsugo/
-// - a message: "made by Joel Foster"
-
-// Using tailwind CSS,  on medium to large screens items will be spaced out horizontally, with the links biased to
-// the left and message biased to the right
-
-// on small screens, items will be stacked vertically, with the links biased to the top and message biased to the bottom
-
 import * as React from "react";
 import { Link } from "gatsby";
 
