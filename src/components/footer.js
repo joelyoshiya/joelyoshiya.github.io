567e3892fa841193d4a327e94159c43df8bfca49
Joel Yoshiya Foster <joel.foster@gmail.com>
2023-06-19 12:54:30 -0500

    simplifying CSS for footer, removing spacing issue on 1st link

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,37 +7,37 @@ const Footer = () => {
       <div className="flex my-2 flex-row space-x-2 items-center justify-center text-sm text-gray-500 dark:text-gray-400 md:flex-row">
         <Link
           to="https://github.com/joelyoshiya"
-          className="my-1 md:my-0 md:mr-5 hover:text-emerald-300"
+          className="hover:text-emerald-300"
         >
           Github
         </Link>
         <Link
           to="https://www.linkedin.com/in/joel-yoshiya-foster/"
-          className="my-1 md:my-0 md:mr-5 hover:text-blue-500"
+          className="hover:text-blue-500"
         >
           LinkedIn
         </Link>
         <Link
           to="mailto:joel.foster@gmail.com"
-          className="my-1 md:my-0 md:mr-5 hover:text-orange-500"
+          className="hover:text-orange-500"
         >
           Email
         </Link>
         <Link
           to="https://drive.google.com/file/d/1n54uFZOBW6t7TRqGt0Tbqtcl7SkU7FqX/view?usp=sharing"
-          className="my-1 md:my-0 md:mr-5 hover:text-green-500"
+          className="hover:text-green-500"
         >
           Resume
         </Link>
         <Link
           to="https://open.spotify.com/user/1229025912?si=2f5006eb6836485f"
-          className="my-1 md:my-0 md:mr-5 text hover:text-green-500"
+          className="text hover:text-green-500"
         >
           Spotify
         </Link>
         <Link
           to="https://letterboxd.com/Mitsugo/"
-          className="my-1 md:my-0 md:mr-5 hover:text-blue-400"
+          className="hover:text-blue-400"
         >
           Letterboxd
         </Link>
